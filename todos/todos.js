a8015b37682e25baa3bad3153ce2118cf8445309
Tamimamim51 <tamimamim151@gmail.com>
2024-11-04 14:07:20 +0600

    first commit

---
+++ todos/todos.js
@@ -0,0 +1,29 @@
+
+function loaddata(){
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(data => displaydata(data))
+    
+  }
+    loaddata();
+  const displaydata = (data) =>{
+    const coldata = document.getElementById("todos");
+        data.forEach((post) => {
+        
+          const postDiv = document.createElement("div");
+
+            postDiv.innerHTML = `
+            
+             <p> tamima
+             </p>
+
+                `;
+             coldata.appendChild(postDiv);         
+
+
+        });
+
+
+  }
+
+
